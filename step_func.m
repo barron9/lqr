c283515e8cc11a96ecfccc50a1b7961d8069c79a
berkin tatlisu <berkin@msn.com>
2025-01-19 19:49:19 +0300

    Update step_func.m

--- step_func.m
+++ step_func.m
@@ -9,3 +9,15 @@ F_s = laplace(f, t, s);
 % Display the result
 disp('Laplace Transform:');
 disp(F_s);
+
+syms t s A t0;
+
+% Define the step function A * 1(t - t0)
+f = A * heaviside(t - t0);
+
+% Compute the Laplace transform
+F_s = laplace(f, t, s);
+
+% Display the result
+disp('Laplace Transform:');
+disp(F_s);
